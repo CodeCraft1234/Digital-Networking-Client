d691e9040e6d2d87a2dfe680c01a7ce33180f079
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-05-25 20:43:16 +0600

    loading spineer added

--- src/Security/PrivateRoute.jsx
+++ src/Security/PrivateRoute.jsx
@@ -7,12 +7,8 @@ const PrivateRoute = ({children}) => {
     const {user,loading}=useContext(AuthContext)
     const location=useLocation()
     if(loading){
-       return <div className="flex items-center justify-center space-x-2">
-       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
-       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
-       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
-   </div>
-    }
+        return <span className="loading loading-spinner text-secondary"></span>;
+      }
     if(user){
         return children;
     }
