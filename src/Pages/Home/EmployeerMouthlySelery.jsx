996ff1d8edb5d4ba004b0e2ecc87bc52c8848678
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-05-29 02:53:59 +0600

    admin page all design finish properly

--- src/Pages/Home/EmployeerMouthlySelery.jsx
+++ src/Pages/Home/EmployeerMouthlySelery.jsx
@@ -17,7 +17,7 @@ const EmployeerMouthlySelery = ({ email }) => {
 
   return (
     <div className="mt-5 p-4 dark:bg-green-800">
-      <h6 className="text-center mb-5 font-bold text-4xl p-1 bg-green-800 text-white">
+      <h6 className="text-center mb-5 font-bold uppercase text-4xl p-2 sm:p-2 bg-green-800 text-white">
         Employees Own Monthly Salary Sheet
       </h6>
       <div className="overflow-x-auto mt-0">
